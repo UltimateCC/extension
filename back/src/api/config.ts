a3ff1f81132f8eb037aa580082d9ba4dc850ac5e
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2023-10-31 02:09:58 +0100

    autoupdate contributor list

--- back/src/api/config.ts
+++ back/src/api/config.ts
@@ -4,9 +4,9 @@ import { dataSource } from "../database";
 import { User, UserConfigSchema, UserSecretsSchema } from "../entity/User";
 import { authMiddleware } from "./auth";
 
-export const apiRouter = Router();
+export const configRouter = Router();
 
-apiRouter.get('/api/config', authMiddleware, async (req, res, next)=>{
+configRouter.get('', authMiddleware, async (req, res, next)=>{
 	try{
 		let user = await dataSource.manager.findOneByOrFail(User, { twitchId: req.session.userid});
 		res.json(user.config);
@@ -15,24 +15,13 @@ apiRouter.get('/api/config', authMiddleware, async (req, res, next)=>{
 	}
 });
 
-apiRouter.post('/api/config', authMiddleware, async (req, res, next)=>{
+configRouter.post('', authMiddleware, async (req, res, next)=>{
 	try{
 		const user = await dataSource.manager.findOneByOrFail(User, {twitchId: req.session.userid});
 		const config = UserConfigSchema.partial().parse(req.body);
 		Object.assign(user.config, config);
 		await dataSource.manager.save(user);
 		res.json(user.config);
-	}catch(e) {
-		next(e);
-	}
-});
-
-apiRouter.post('/api/secrets', authMiddleware, async (req, res, next)=>{
-	try{
-		const user = await dataSource.manager.findOneByOrFail(User, {twitchId: req.session.userid});
-		Object.assign(user.secrets, UserSecretsSchema.parse(req.body));
-		await dataSource.manager.save(user);
-		res.json({success: true});
 	}catch(e) {
 		next(e);
 	}
