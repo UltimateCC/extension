5b9c571e4011e7ded81f4b927b682efe73902fb2
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2023-11-23 00:07:04 +0100

    stats api

--- back/src/api/stats.ts
+++ back/src/api/stats.ts
@@ -5,17 +5,20 @@ const admins = (process.env.ADMINS_TWITCHID ?? '').split(',');
 
 export const statsRouter = Router();
 
-// Basic way to get all statistics
-// Will probably change soon (pagination + users can get their own stats)
 statsRouter.get('', async (req, res, next)=>{
 	try{
-		if(!admins.includes(req.session.userid!)) {
-			res.status(403).json({
-				error: 'Forbidden'
-			});
-			return;
+		// If user is admin and "all" parameter is set, get stats from all users
+		let all = false;
+		if(req.query.all) {
+			if(!admins.includes(req.session.userid!)) {
+				res.status(403).json({
+					error: 'Forbidden'
+				});
+				return;
+			}
+			all = true;
 		}
-		const stats = await Stats.find();
+		const stats = await Stats.findBy({ twitchId: all ? undefined : req.session.userid });
 		res.json(stats);
 	}catch(e) {
 		next(e);
