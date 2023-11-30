b91e270f84d1f2bae7cc1f042becb6451fbf6f46
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2023-11-30 15:43:02 +0100

    move sessionmiddleware

---
+++ back/src/middleware/session.ts
@@ -0,0 +1,38 @@
+import session from "express-session";
+import { config } from "../config";
+import { logger } from "../logger";
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
+
+// Sessions are valid for 24h
+const sessionTime = 60 * 60 * 24;
+
+const redisClient = createClient({
+	url: config.SESSION_REDIS
+});
+redisClient.on('error', e=>{
+	logger.error('Redis client error', e);
+});
+
+export const sessionMiddleware = session({
+	proxy: true,
+	secret: config.SESSION_SECRET,
+	saveUninitialized: false,
+	resave: false,
+	cookie: {
+		//secure: true,
+		maxAge: sessionTime * 1000
+	},
+	store: new RedisStore({
+		client: redisClient,
+		prefix: "captions:",
+	})
+});
+
+export async function initSessionMiddleware() {
+	await redisClient.connect();
+}
+
+export async function stopSessionMiddleware() {
+	await redisClient.disconnect();
+}
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,45 +1,18 @@
 
 import express from 'express';
-import session from 'express-session';
 import { createServer } from 'http';
-import RedisStore from "connect-redis";
-import {createClient} from "redis";
 import { Server } from 'socket.io';
 import { initSocketioServer, endSocketSessions } from './socketioServer';
 import { apiRouter } from './api/apiRoutes';
 import { rateLimiterMiddleware } from './middleware/rateLimit';
 import { config } from './config';
 import { logger } from './logger';
+import { initSessionMiddleware, sessionMiddleware, stopSessionMiddleware } from './middleware/session';
 
 const app = express();
 app.set('trust proxy', 1);
 
 app.use(rateLimiterMiddleware);
-
-// Sessions are valid for 24h
-const sessionTime = 60 * 60 * 24;
-
-const redisClient = createClient({
-	url: config.SESSION_REDIS
-});
-redisClient.on('error', e=>{
-	logger.error('Redis client error', e);
-});
-
-const sessionMiddleware = session({
-	proxy: true,
-	secret: config.SESSION_SECRET,
-	saveUninitialized: false,
-	resave: false,
-	cookie: {
-		//secure: true,
-		maxAge: sessionTime * 1000
-	},
-	store: new RedisStore({
-		client: redisClient,
-		prefix: "captions:",
-	})
-});
 app.use(sessionMiddleware);
 app.use(express.json());
 
@@ -49,26 +22,16 @@ app.use('/api', apiRouter);
 // socket.io on same server
 export const server = createServer(app);
 export const io = new Server(server);
-/*
-// socketio admin UI
-instrument(io, {
-	auth: {
-		type: 'basic',
-		username: 'admin',
-		password: process.env.SOCKETIO_ADMIN_PASSWORD!
-	}
-});*/
+
 io.engine.use(sessionMiddleware);
 //Register socketio routes
 initSocketioServer(io);
 
 
-const PORT = config.PORT;
-
 export async function startServer() {
-	await redisClient.connect();
+	await initSessionMiddleware();
 	await new Promise<void>((res)=>{
-		server.listen(PORT, ()=>{
+		server.listen(config.PORT, ()=>{
 			res();
 		});
 	});
@@ -85,6 +48,6 @@ export async function stopServer() {
 		}),
 		endSocketSessions(io)
 	]);
-	await redisClient.disconnect();
 	logger.info('Server closed');
+	await stopSessionMiddleware();
 }
