8704964107c86d8dbedf76e8d8450027eba79c15
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2023-12-06 18:18:41 +0100

    recommend overlay in guide*

--- website/src/components/Guide/index.tsx
+++ website/src/components/Guide/index.tsx
@@ -5,15 +5,15 @@ function Guide() {
 		<div className="guide">
 			<h4>To setup captions on your stream, follow the instructions below</h4>
 			<ul>
-				<li>Ensure you have installed the <a href={config.twitch}>Twitch extension</a></li>
-				<li>Ensure you have selected the correct spoken language</li>
+				<li>Install the <a href={config.twitch}>Twitch extension</a> (We recommend overlay)</li>
+				<li>Select your spoken language</li>
 				<li>Start captions by clicking "Start listening"</li>
 				<li>Accept microphone permission if necessary, and check if correct microphone is selected</li>
 				<li>Keep this window open during your stream</li>
 				<li>Optional: Check other tabs for further configuration</li>
 			</ul>
 			<div className="help">
-				If you have any issue with the extension, join the <a href={config.discord}>discord</a> !
+				If you have any issue with the extension, join the <a href={config.discord}>discord</a>&nbsp;!
 			</div>
 		</div>
 	);
