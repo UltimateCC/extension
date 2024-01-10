f8be859da360955dfa33c94f2a0db8029e839fa8
Sytorex-pro <hydro.tex27@gmail.com>
2024-01-10 20:14:42 +0100

    Edit config page

--- extension/src/components/Config.svelte
+++ extension/src/components/Config.svelte
@@ -8,7 +8,7 @@
 	<div>
 		<h2>Thanks for installing <a href={url} target="_blank" rel="noreferrer">Ultimate Closed Captions</a></h2>
 		<p>
-			For a better viewer experience, install this extension as an overlay
+			For a better viewer experience, install this extension as an <strong>overlay</strong>
 		</p>
 		<p>
 			To work correctly, this extension requires you to use the dashboard at
@@ -18,9 +18,10 @@
 </main>
 
 <style>
-	main { background-color: #37373E; color: #D9D9D9; font-family: Arial, Helvetica, sans-serif; font-size: 1em; height: 100%; }
+	main { background-color: #37373E; color: #D9D9D9; font-family: Arial, Helvetica, sans-serif; font-size: 1.5em; height: 100%; }
 	main { display: flex; align-items: center; justify-content: center; padding: 5em 2em; }
-	h2 { margin-bottom: .5em; }
+	h2 { margin-bottom: 1.5em; }
 	div { text-align: center; }
-	a { color: #B072E8; }
+	a, strong { color: #B072E8; }
+	p { margin-bottom: 0.5em; }
 </style>
