a01a2fb1272b6661bac24487dff5681509e473b5
Sytorex-pro <hydro.tex27@gmail.com>
2024-02-22 18:06:30 +0100

    Update github link

--- website/src/config.ts
+++ website/src/config.ts
@@ -1,6 +1,6 @@
 
 export const config = {
 	twitch: 'https://dashboard.twitch.tv/extensions/vaa54br1ykkbm45gqstdu81m2d66hz',
-	github: 'https://github.com/Sytorex-pro/UltimateClosedCaptions',
+	github: 'https://github.com/UltimateCC/UltimateClosedCaptions',
 	discord: 'https://discord.gg/f8fUqHwtHx'
 }
