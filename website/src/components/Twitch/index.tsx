96b23559cd75525997ed9545d02dadb7c9bb18e5
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2024-03-07 13:46:24 +0100

    hide disabled settings

--- website/src/components/Twitch/index.tsx
+++ website/src/components/Twitch/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useSavedConfig } from '../../hooks/useSavedConfig';
 import ConfigSwitch from '../ConfigSwitch';
 
@@ -12,14 +12,14 @@ interface TwitchConfig {
     showCaptions: boolean
 }
 
-function Twitch({ customDelayConfig, updateConfig, twitchAutoStop }: TwitchProps) {
-	const timeoutCustomDelay = useRef<ReturnType<typeof setTimeout>>();
+function Twitch({ /*customDelayConfig,*/ updateConfig, twitchAutoStop }: TwitchProps) {
+	//const timeoutCustomDelay = useRef<ReturnType<typeof setTimeout>>();
 
     // Twitch config
     const {
-        config: twitchConfig,
+        //config: twitchConfig,
         loadConfig: loadTwitchConfig,
-        updateConfig: updateTwitchConfig
+        //updateConfig: updateTwitchConfig
     } = useSavedConfig<TwitchConfig>({apiPath: 'twitchconfig'});
 
     useEffect(()=>{
@@ -29,12 +29,13 @@ function Twitch({ customDelayConfig, updateConfig, twitchAutoStop }: TwitchProps
         });
     }, [loadTwitchConfig]);
 
+    /*
     const handleShowCaptionsChange = (val: boolean) => {
         updateTwitchConfig({ showCaptions: val })
         .catch(err => {
             console.error('Error updating Twitch config', err);
         });
-    }
+    }*/
 
     const handleAutoStopChange = (val: boolean) => {
         updateConfig({ twitchAutoStop: val })
@@ -43,32 +44,34 @@ function Twitch({ customDelayConfig, updateConfig, twitchAutoStop }: TwitchProps
         });
     }
 
+    /*
     const updateCustomDelay = (event: React.ChangeEvent<HTMLInputElement>) => {
-		// Max 1s per update
-		clearTimeout(timeoutCustomDelay.current);
-		timeoutCustomDelay.current = setTimeout(()=>{
-			const customDelay = Number.parseFloat(event.target.value);
-			if(!Number.isNaN(customDelay) && customDelay !== customDelayConfig && customDelay >= -5 && customDelay <= 1800) {
-				updateConfig({ customDelay })
-				.catch(err => {
-					console.error('Error updating config', err);
-				});
-			} else {
-				if (customDelay < -5) event.target.value = "-5";
-				else if (customDelay > 1800) event.target.value = "1800";
-			}
-
-		}, 500);
-	};
+        // Max 1s per update
+        clearTimeout(timeoutCustomDelay.current);
+        timeoutCustomDelay.current = setTimeout(()=>{
+            const customDelay = Number.parseFloat(event.target.value);
+            if(!Number.isNaN(customDelay) && customDelay !== customDelayConfig && customDelay >= -5 && customDelay <= 1800) {
+                updateConfig({ customDelay })
+                .catch(err => {
+                    console.error('Error updating config', err);
+                });
+            } else {
+                if (customDelay < -5) event.target.value = "-5";
+                else if (customDelay > 1800) event.target.value = "1800";
+            }
+        }, 500);
+    };*/
 
     return (
         <div className="twitch">
             <div className="twitch-switch-container">
-                <ConfigSwitch
-                    checked={ twitchConfig.showCaptions ?? true }
-                    onChange={handleShowCaptionsChange}
-                    label="Show captions to viewers by default"
-                />
+                {/*
+                    <ConfigSwitch
+                        checked={ twitchConfig.showCaptions ?? true }
+                        onChange={handleShowCaptionsChange}
+                        label="Show captions to viewers by default"
+                    />                
+                */}
                 <ConfigSwitch
                     checked={twitchAutoStop}
                     onChange={handleAutoStopChange}
@@ -76,17 +79,19 @@ function Twitch({ customDelayConfig, updateConfig, twitchAutoStop }: TwitchProps
                 />
             </div>
 
-            <div className="custom-delay">
-                <p>
-                    Add delay to your captions to match your stream delay.
-                </p>
-                <div className="custom-delay-input">
-                    <label>
-                        Custom delay (in secondes)
-                        <input className="theme-input" type="number" min="-5" max="1800" defaultValue={customDelayConfig} onChange={updateCustomDelay} step=".1" />
-                    </label>
+            {/*
+                <div className="custom-delay">
+                    <p>
+                        Add delay to your captions to match your stream delay.
+                    </p>
+                    <div className="custom-delay-input">
+                        <label>
+                            Custom delay (in secondes)
+                            <input className="theme-input" type="number" min="-5" max="1800" defaultValue={customDelayConfig} onChange={updateCustomDelay} step=".1" />
+                        </label>
+                    </div>
                 </div>
-            </div>
+            */}
         </div>
     );
 }
