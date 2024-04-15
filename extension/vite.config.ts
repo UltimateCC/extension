5612855c7a106cde15b7a7502d7baf1245a45cb2
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2024-04-15 21:38:40 +0200

    extension cert file location

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -4,7 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), basicSsl()],
+  plugins: [svelte(), basicSsl({ certDir: './cert' })],
   server: {
     port: 5174
   },
