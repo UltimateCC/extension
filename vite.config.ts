1f9e10ba27fc0cadb291aaf4968cff00bef2213b
Salutations45 <48554716+Salutations45@users.noreply.github.com>
2024-06-24 15:35:06 +0200

    vite server use polling

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [svelte(), basicSsl({ certDir: './cert' })],
   server: {
-    port: 5174
+    port: 5174,
+		watch: {
+			usePolling: true
+		}
   },
   base: './',
   build: {
